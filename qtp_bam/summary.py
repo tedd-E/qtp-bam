33c81b1ec8327b55804ecebe859a282fa46a194a
tedd-E <62875128+tedd-E@users.noreply.github.com>
2022-08-17 22:45:25 -0700

    first attempt summary

--- qtp_bam/summary.py
+++ qtp_bam/summary.py
@@ -13,6 +13,35 @@ import pysamstats
 import matplotlib.pyplot as plt
 from os.path import join
 
+"""
+def summary(qclient, job_id, parameters, out_dir)
+
+qclient = qiita client instance (use for getting "res" (see biom test_validate line 52?)
+job id = string (not important)
+parameters = {'template': 1,
+              'files': dumps(filepaths),
+              'artifact_type': 'BIOM',
+              'analysis': 1}
+out_dir = mkdtemp()   (outputs files to a temp dir) 
+
+NOTE that dumps(filepaths) is the JSON string format of filepaths, where...
+filepaths = {'biom': [join(fp_support_files, 'sepp.biom')],
+                     'preprocessed_fasta': [join(fp_support_files, 'sepp.fa')],
+                     'plain_text': [join(fp_support_files, 'sepp.tre')]}
+aka 
+    filepaths = {
+                'biom': [fp_support_files/sepp.biom],
+                 'preprocessed_fasta': [fp_support_files/sepp.fa')],
+                 'plan_text': [join(fp_support_files/sepp.tre')]
+                 }
+such that...
+parameters = {'template': 1,
+              'files': {'biom': [join(fp_support_files, 'sepp.biom')],
+                     'preprocessed_fasta': [join(fp_support_files, 'sepp.fa')],
+                     'plain_text': [join(fp_support_files, 'sepp.tre')]},
+              'artifact_type': 'BIOM',
+              'analysis': 1}
+"""
 # TODO: look at step 7/8 of notes for BAM summary generation!
 def generate_html_summary(qclient, job_id, parameters, out_dir):
     """Generates the HTML summary of an artifact
@@ -46,20 +75,25 @@ def generate_html_summary(qclient, job_id, parameters, out_dir):
     artifact_files = artifact_info['files']    # Get the artifact files
 
     # Step 2: generate HTML summary
-    # TODO: Generate the HTML summary and store it in html_summary_fp
-    # TODO: what summary do you want from a bam file. do we need a summary
-    # NOTE: just writing samtool stats to html for now
     qclient.update_job_step(job_id, "Step 2: Generating HTML summary")
+    # samtools stats <file.bam> | grep "is sorted:"
+    # samtools flagstat test.bam (gets summary)
+
+    # TODO: check that artifact_files iteration is correct
+    # NOTE: for now, assumes bam
+    artifact_information = "--BAM SUMMARY,--"
+    for bamfile in artifact_files:
+        artifact_information += '\n' + str(pysam.flagstat(bamfile))
 
     # code from https://pypi.org/project/pysamstats/0.14/
-    mybam = pysam.Samfile(artifact_files[0])
-    a = pysamstats.load_coverage(mybam, chrom='Pf3D7_01_v3', start=10000, end=20000)
-    plt.plot(a.pos, a.reads_all)
-    plot = BytesIO()
-    plt.savefig(plot, format='png')
-    artifact_information = [(
-        '<img src = "data:image/png;base64,{}"/>'.format(
-            b64encode(plot.getvalue()).decode('utf-8')))]
+    # mybam = pysam.Samfile(artifact_files[0])
+    # a = pysamstats.load_coverage(mybam, chrom='Pf3D7_01_v3', start=10000, end=20000)
+    # plt.plot(a.pos, a.reads_all)
+    # plot = BytesIO()
+    # plt.savefig(plot, format='png')
+    # artifact_information = [(
+    #     '<img src = "data:image/png;base64,{}"/>'.format(
+    #         b64encode(plot.getvalue()).decode('utf-8')))]
 
     of_fp = join(out_dir, "artifact_%d.html" % artifact_id)
     with open(of_fp, "w") as summaryfile:
